bbaa5da1b0fab70a34d45297d4869fd7af3c0f99
Ezequiel Cuenga <ezequielcuenga@gmail.com>
2022-07-02 00:13:23 -0300

    Aprendendo Python

---
+++ ex005-format.py
@@ -0,0 +1,18 @@
+print('\033[1;33mNessa aula, vamos aprender quais são os operadores aritméticos do Python e também sua ordem de '
+      '\nprecedência dentro de expressões matemáticas. Veja como funcionam os operadores de adição, subtração, '
+      '\nmultiplicação, divisão, exponenciação e quociente na linguagem Python.\033[m')
+
+print('Operadores Aritméticos')
+#5 + 2 * 3 == 11
+#3 * 5 + 4 ** 2 #== 31
+#3 + (5 + 4) ** 2 #== 243
+
+nome = input('Qual o seu nome: ')
+print('Prazer em te conhecer {}!'.format(nome))
+print('Prazer em te conhecer {:20}!'.format(nome))
+print('Prazer em te conhecer {:>20}!'.format(nome))
+print('Prazer em te conhecer {:<20}'.format(nome))
+print('Prazer em te conhecer {:^20}!'.format(nome))
+print('Prazer em te conhecer {:=^20}!'.format(nome))
+
+
